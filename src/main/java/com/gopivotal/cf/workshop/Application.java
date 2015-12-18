ec2bdf97f57a52dff640f7fd8c23ac614aa2d331
shyam327 <rshyam@gmail.com>
2015-12-18 14:12:38 -0600

    Update Application.java

--- src/main/java/com/gopivotal/cf/workshop/Application.java
+++ src/main/java/com/gopivotal/cf/workshop/Application.java
@@ -10,6 +10,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Config.class, args);
+        
+    /*
+    Magic !!
+    */
     }
     
 }
